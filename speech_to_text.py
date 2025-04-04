3ca21e926fa5a63592e1f1f5fdf7a99dba6ee539
JanW42 <Jan.Wobker12@web.de>
2025-04-04 10:39:33 +0200

    Version 1.4

--- speech_to_text.py
+++ speech_to_text.py
@@ -2,6 +2,8 @@ import sys
 import os
 from pathlib import Path
 from faster_whisper import WhisperModel
+from settings import settings #Lokal eigene Lib
+from icecream import ic
 
 #pip install faster-whisper
 #pip install pyaudio
@@ -19,7 +21,7 @@ def set_cuda_paths():
         current_value = os.environ.get(env_var, '')
         new_value = os.pathsep.join(paths_to_add + [current_value] if current_value else paths_to_add)
         os.environ[env_var] = new_value
-    print("CUDA_PFAD wurde erfolgreich gesetzt")
+    #print("CUDA_PFAD wurde erfolgreich gesetzt")
     
 #pip install torch==2.6.0
 #pip install torchaudio==2.6.0
@@ -29,10 +31,12 @@ def set_cuda_paths():
 #pip install nvidia-cublas-cu12==12.4.5.8
 
 def speech_to_text_processer():
-    model_size = "large-v3"
+    ic()
+    print("Sprache zu Text starten")
+    model_size = settings.model_size
 
     # Run on GPU with FP16
-    model = WhisperModel(model_size, device="cuda", compute_type="float16")
+    model = WhisperModel(model_size, device=settings.device, compute_type=settings.compute_type)
 
     # or run on GPU with INT8
     # model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
@@ -41,10 +45,12 @@ def speech_to_text_processer():
 
     segments, info = model.transcribe("input.wav", beam_size=5)
 
-    print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
+    #print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
 
     for segment in segments:
         print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
+    ic()
+    print("Sprache zu Text fertig")
     return segment.text
 
 def Speech_to_Text_Parser():
@@ -52,8 +58,8 @@ def Speech_to_Text_Parser():
         text = speech_to_text_processer()
         #print("Speech_to_text_processer_successfully")
     except Exception as e:
-        #print ("Nicht Erfolgreiches Sprache zu Text")
-        pass
+        print (e)
+        raise RuntimeError("Exception in first playsound")
     return text 
 
 
