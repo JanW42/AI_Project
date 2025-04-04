1eee8c8324cc17f077ea1d6e40b7795266530c9f
JanW42 <Jan.Wobker12@web.de>
2025-04-04 10:39:12 +0200

    Version 1.4

--- main.py
+++ main.py
@@ -3,58 +3,46 @@ import re
 import time
 import asyncio
 import edge_tts #pip install edge_tts
-from config import config
+from config import config #Lokal eigene Lib
+from settings import settings #Lokal eigene Lib
 from openai import AzureOpenAI #pip install openai
 from playsound import playsound #pip install playsound==1.2.2
-from audio_recorder import record_and_save, get_ambient_noise
-from speech_to_text import Speech_to_Text_Parser, set_cuda_paths
+from audio_recorder import record_and_save, get_ambient_noise #Lokal eigene Lib
+from speech_to_text import Speech_to_Text_Parser, set_cuda_paths #Lokal eigene Lib
+from icecream import ic
 
-text = "Hallo mein Name ist Lucy. Ich bin eine künstliche Intelligenz. Wie kann ich dir helfen?"
-filename  = "output.mp3"
-voice = "de-DE-SeraphinaMultilingualNeural"
-rate="+10%"
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-
-for root, dirs, files in os.walk(dir_path):
-    for file in files:
-        if file.endswith('output.mp3'):
-            print (root+'/'+str(file))
-            os.remove(filename)
-
-async def text_to_mp3(text,filename,voice,rate):
-    communicate = edge_tts.Communicate(text, voice, rate=rate)
-    await communicate.save(filename)
+def initial_path():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    for file in os.listdir(dir_path):  # Nur Dateien im Root-Ordner auflisten
+        file_path = os.path.join(dir_path, file) #stellt sicher, dass richtiges Trennzeichen MacOs Windows
+        if file.endswith(settings.filename):   #zur wav datei machen für mehr performance!
+            print (file_path+'/'+str(file))
+            os.remove(settings.filename) #Datei löschen wenn mp3 vorhanden
+            ic()
+            
+## async ist eine besondere Funktion. Asynchrones Programmieren. Nicht so wie das Normale synchrone was codezeile nach codezeile in einer Abfolge abarbeitet. Hier braucht man auch besondere Syntax, async und await. async initialisiert und await lässt der Code an der stelle stehen und es werden keine neuen Ressourcen freigeben. Ist fortgeschrittnes Python programmieren braucht man aber hier. Threding hilfreich zu verstehen um das zu verstehen.
+async def text_to_mp3(welcometext,filename,voice,rate):
+    ic()
+    print("Text zu Mp3 starten")
+    communicate = edge_tts.Communicate(welcometext, voice, rate=rate)
+    await communicate.save(settings.filename)  ## warte bis file gespeichert bevor codezeile verlassen wird.
     #print(f"Saved {filename}")
+    ic()
+    print("Text zu Mp3 gespeichert")
 
-def remove_asterisks(text: str) -> str:
-    """
-    Entfernt alle Sternchen (*) aus dem gegebenen String mit regulären Ausdrücken.
+def remove_asterisks(welcometext):
+    #Entfernt alle Sternchen (*) und (-) aus dem GPT content.
+    # return: String ohne * und -
+    return re.sub(r'[\*-]', '', welcometext)
     
-    :param text: Eingabestring
-    :return: String ohne *
-    """ 
-    return re.sub(r'[\*-]', '', text)
-  
 def create_message(prompt):
-    model = "gpt4o"   #definiere Model von ChatGpt
+    ic()
+    print("Text zu Audio fertig")
+    model = settings.model   #definiere Model von ChatGpt
 
     #Nachfolgende OpenAIModel und if Funktion Optional
-    OpenAIModels = {
-        "gpt4o": "gpt4o",
-        "gpt-4": "gpt-4",
-        "gpt-4-turbo": "gpt-4-turbo",
-        "gpt-3.5-turbo": "gpt-3.5-turbo",
-        "text-davinci-003": "text-davinci-003",
-        "text-embedding-3-large": "embed",      # Große Vektor (z. B. 1536 Dimensionen)
-        "text-embedding-3-small": "embeddings", # Kleine Vektor (z. B. 384 Dimensionen)
-        "dall-e-3": "dall-e-3",
-        "whisper-1": "whisper-1",
-        "o1": "o1",
-        "o1-mini": "o1-mini",
-        "o3": "o3",
-        "o3-mini": "o3-mini"
-    }
+    OpenAIModels = settings.OpenAIModels
     
     if model not in OpenAIModels:
         raise ValueError(f"Das Modell {model} wird nicht unterstützt. "
@@ -79,29 +67,44 @@ def create_message(prompt):
     #Frage nach bei der API. Sende dafür die Nachricht mit dem GPT Model was zur Antwort benutzt werden soll.
     response = client.chat.completions.create(messages=messages, model=model)
     response_content = response.choices[0].message.content
+    ic()
+    print("Zeichen aus der GPT_Antwort Anfang")
     response_without_asterisks = remove_asterisks(response_content) #Sonderzeichen die im GPT Output stehen entfernen für bessere Sprachausgabe
+    ic()
+    print("Zeichen aus der GPT_Antwort Ende")
+    ic()
+    print("Antwort von GPT_API")
+    print (response_without_asterisks)
     return response_without_asterisks
 
-asyncio.run(text_to_mp3(text, filename, voice, rate))
-try:
-    playsound(filename)
-    os.remove(filename)
-except Exception as e:
-    pass
 
-set_cuda_paths ()# Setzte einmal den CUDA Pfad
-get_ambient_noise()# Erfasse Hintergrundrauschen
-
-while True:
+if __name__ == "__main__":
+    initial_path()
+    asyncio.run(text_to_mp3(settings.welcometext, settings.filename, settings.voice, settings.rate))
     try:
-        record_and_save() #Hier wird die Funktion record and save aufgerufen um die Mikrosprache solange auszunehmen bis man aufhört zu reden. Dann wird es in der Input.wav Datei gespeichert.
-        text = Speech_to_Text_Parser() #Hier wird die Sprache aus input.mp3 in Text verarbeitet mit der extrem Leistungsstarken lokalen CUDA anwendung von OpenAI / Nvidia
-        result = create_message(text)  #Rufe die Funktion auf und übergebe die "Frage" zu ChatGpt API
-        asyncio.run(text_to_mp3(result, filename, voice, rate))
-        try:
-            playsound(filename)
-            os.remove(filename)
-        except Exception as e:
-            pass
+        playsound(settings.filename)
+        os.remove(settings.filename)
     except Exception as e:
-        pass
+        print (e)
+        raise RuntimeError("Exception in first playsound")
+
+    set_cuda_paths()# Setzte einmal den CUDA Pfad
+    get_ambient_noise()# Erfasse Hintergrundrauschen
+
+    while True:
+        try:
+            record_and_save() #Hier wird die Funktion record and save aufgerufen um die Mikrosprache solange auszunehmen bis man aufhört zu reden. Dann wird es in der Input.wav Datei gespeichert.
+            text = Speech_to_Text_Parser() #Hier wird die Sprache aus input.mp3 in Text verarbeitet mit der extrem Leistungsstarken lokalen CUDA anwendung von OpenAI / Nvidia
+            result = create_message(text)  #Rufe die Funktion auf und übergebe die "Frage" zu ChatGpt API
+            asyncio.run(text_to_mp3(result, settings.filename, settings.voice, settings.rate))
+            try:
+                ic()
+                print("Audio Datei fertig gleich abspielen")
+                playsound(settings.filename) # Den im neuem Thread starten um dazwischen zu sprechen
+                os.remove(settings.filename)
+            except Exception as e:
+                print (e)
+                raise RuntimeError("Exception in playsound in While True")
+        except Exception as e:
+            print (e)
+            raise RuntimeError("Exception in Haupt While True")
