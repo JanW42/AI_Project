c9ed0252f86fc72b4ac1aafb8f4bf49f359bcd64
JanW42 <Jan.Wobker12@web.de>
2025-04-13 21:37:03 +0200

    Version 1.1

--- text_to_speech.py
+++ text_to_speech.py
@@ -11,7 +11,7 @@ def remove_asterisks(welcometext):
     return re.sub(r'[\*-]', '', welcometext)
 
 ## async ist eine besondere Funktion. Asynchrones Programmieren. Nicht so wie das Normale synchrone was codezeile nach codezeile in einer Abfolge abarbeitet. Hier braucht man auch besondere Syntax, async und await. async initialisiert und await lässt der Code an der stelle stehen und es werden keine neuen Ressourcen freigeben. Ist fortgeschrittnes Python programmieren braucht man aber hier. Threding hilfreich zu verstehen um das zu verstehen.
-async def text_to_mp3(welcometext,filename,voice,rate):
-    communicate = edge_tts.Communicate(welcometext, voice, rate=rate)
+async def text_to_mp3(welcometext,filename,voice,rate,pitch):
+    communicate = edge_tts.Communicate(welcometext, voice, rate=rate, pitch=pitch)
     await communicate.save(settings.filename)  ## warte bis file gespeichert bevor codezeile verlassen wird.
     #print(f"Saved {filename}")
